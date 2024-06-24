ddbe79b7303efcad797de1a14e284ffed53a697a
Yash Kashte <72797164+Yashkashte5@users.noreply.github.com>
2024-06-24 20:31:02 +0530

    Delete python programming folder

--- Python Programming/extract.py
+++
@@ -1,7 +0,0 @@
-import numpy as n
-arr = n.array([10,2,3,4,5,6,20,45])
-
-#Extract the elements greater than 5
-
-extracted_elements = n.extract(arr > 5, arr)
-print("Elements greater than 5 are : ", extracted_elements)
--- Python Programming/reverse.py
+++
@@ -1,3 +0,0 @@
-numbers = (1, 2, 3, 4, 5)
-reversed_numbers = numbers[::-1]
-print("Reversed Tuple:", reversed_numbers)
